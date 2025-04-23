bca989109861fa8ee1d7e6c702854ba84a13e98f
sonu072805 <sonusingh072805@gmail.com>
2025-04-23 14:51:22 +0000

    done the error

---
+++ 02_basics/one.js
@@ -0,0 +1,19 @@
+const myNum = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// myNum.forEach( (num) => console.log(num + 10) )
+
+// ----------------------------------------------
+
+const coding = [ "java", "ruby", "python"]
+
+coding.forEach( (item) => (item) )
+// console.log(coding);
+
+// -----------------------------------------------------
+
+const myNumbr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const value = myNumbr.map( (num) => (num + 10) )
+
+console.log(value);
+
